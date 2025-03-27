2964a016649d8ed86b057c27cbdf8678b3c74a18
Yash <67926590+Yash094@users.noreply.github.com>
2025-03-27 23:36:55 +0530

    add repro

--- index.ts
+++ index.ts
@@ -1,75 +1,47 @@
-import { config } from "dotenv";
-import { readFileSync } from "fs";
-import {
-  createThirdwebClient,
-  getContract,
-  sendAndConfirmTransaction,
-} from "thirdweb";
-import { polygonAmoy } from "thirdweb/chains";
-import { deployERC721Contract } from "thirdweb/deploys";
-import { lazyMint } from "thirdweb/extensions/erc721";
-import { privateKeyToAccount } from "thirdweb/wallets";
+import { createThirdwebClient } from "thirdweb";
+import { upload } from "thirdweb/storage";
+import fs from "fs"
+import path from "path"
+import { File } from "buffer";
 
-config();
-
-const main = async () => {
-  if (!process.env.WALLET_PRIVATE_KEY) {
-    throw new Error("No private key found");
-  }
-  if (!process.env.THIRDWEB_SECRET_KEY) {
-    throw new Error("No THIRDWEB_SECRET_KEY found");
-  }
-  try {
-    const chain = polygonAmoy;
+async function main() {
     const client = createThirdwebClient({
-      secretKey: process.env.THIRDWEB_SECRET_KEY,
+        clientId: "",
+        config: {
+            storage: {
+                fetch: {
+                    // set the timeout for NFTs upload to 10 minutes
+                    requestTimeoutMs: 60 * 1000 * 10,
+                },
+            },
+        },
     });
-    const account = privateKeyToAccount({
-      client,
-      privateKey: process.env.WALLET_PRIVATE_KEY,
-    }); // private key account
-    const address = await deployERC721Contract({
-      chain,
-      client,
-      account,
-      type: "DropERC721",
-      params: {
-        name: "My Drop",
-        symbol: "MYNFT",
-      },
-    });
-    console.log("Contract address: ", address);
-    const contract = getContract({ address, chain, client });
-    const nfts = [
-      {
-        name: "Blue Star",
-        description: "A blue star NFT",
-        image: readFileSync("assets/blue-star.png"),
-      },
-      {
-        name: "Red Star",
-        description: "A red star NFT",
-        image: readFileSync("assets/red-star.png"),
-      },
-      {
-        name: "Yellow Star",
-        description: "A yellow star NFT",
-        image: readFileSync("assets/yellow-star.png"),
-      },
-    ];
-    const transaction = lazyMint({
-      contract: contract,
-      nfts,
-    });
-    const data = await sendAndConfirmTransaction({
-      transaction,
-      account,
-    });
-    console.log("Lazy minted successfully!");
-    console.log(`Transaction hash: ${data.transactionHash}`);
-  } catch (err) {
-    console.error("Something went wrong: ", err);
-  }
-};
+    const filePath = path.join(__dirname, "4k_Thetestdata.mp4");
+    const fileBuffer = fs.readFileSync(filePath); // Read file as buffer
+    const file = new File([fileBuffer], "4k_Thetestdata.mp4", { type: "video/mp4" });
 
-main();
+    const filePath2 = path.join(__dirname, "4k_Thetestdata copy.mp4");
+    const fileBuffer2 = fs.readFileSync(filePath2); // Read file as buffer
+    const file2 = new File([fileBuffer2], "4k_Thetestdata copy.mp4", { type: "video/mp4" });
+
+    const filePath3 = path.join(__dirname, "4k_Thetestdata copy 2.mp4");
+    const fileBuffer3 = fs.readFileSync(filePath3); // Read file as buffer
+    const file3 = new File([fileBuffer3], "4k_Thetestdata copy 2.mp4", { type: "video/mp4" });
+    console.time("Uploading file...");
+    try {
+        
+        const uris = await upload({
+            client,
+            files: [file, file2, file3] // Create a Blob],
+
+        });
+       
+        console.log("Uploaded file URIs:", uris);
+    } catch (e) {
+        console.log(e)
+    }
+    console.timeEnd("Uploading file...");
+
+}
+
+main()
